9fa4423d9a60ade62abcb4de88e09a641c9638b3
hohosznta <ninetale22@gmail.com>
2025-06-04 17:04:32 +0900

    change staria api

--- src/main/java/com/ll/carjini/domain/maintenanceHistory/repository/MaintenanceHistoryRepository.java
+++ src/main/java/com/ll/carjini/domain/maintenanceHistory/repository/MaintenanceHistoryRepository.java
@@ -9,8 +9,6 @@ import java.util.Optional;
 
 public interface MaintenanceHistoryRepository extends JpaRepository<MaintenanceHistory, Long> {
 
-    List<MaintenanceHistory> findByMaintenanceItemId(Long maintenanceItemId);
-
     Optional<MaintenanceHistory> findTopByMaintenanceItemIdOrderByReplacementDateDescReplacementKmDesc(Long id);
 
     void deleteByMaintenanceItem(MaintenanceItem item);
